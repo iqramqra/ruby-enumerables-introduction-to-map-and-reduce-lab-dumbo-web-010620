cb9ad1990e9721390b4a76bad8dffbf14b696a66
Iqra Masroor <masrooriqra@gmail.com>
2019-12-12 20:04:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -31,7 +31,7 @@ def map_to_square(source_array)
   new_array=[]
   i=0
   while i<source_array.length do
-    new_array.push(source_array[i]**2)
+    new_array.push(source_array[i]*i)
     i+=1
   end
   return new_array
