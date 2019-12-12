b5aa8eb344b86d663f686615cf751bb8d774c024
Iqra Masroor <masrooriqra@gmail.com>
2019-12-12 20:07:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,11 +9,11 @@ return new_array
 end
 
 def map_to_no_change(source_array)
-new_array = []
-i = 0
-while i < source_array.length do
+new_array=[]
+i=0
+while i<source_array.length do
   new_array.push(source_array[i])
-  i +=1
+  i+=1
 
 end
   return new_array
