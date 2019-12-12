a6cdf82491358f4f2cb3071d11d07a0151224812
Iqra Masroor <masrooriqra@gmail.com>
2019-12-12 20:04:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -37,15 +37,4 @@ def map_to_square(source_array)
   return new_array
 end
 
-def reduce_to_total(source_array, starting_point=0)
-  total=starting_point
-  i=0
-  while i<source_array.length do
-    tota+= source_array[i]
-    i+=1
-  end
-  return total
-end
-
-
 
