74c760eb6485d9c9bd5768519b1bb87d5dc22f20
Iqra Masroor <masrooriqra@gmail.com>
2019-12-12 20:07:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,11 +20,11 @@ end
 end
 
 def map_to_double(source_array)
-new_array = []
-i = 0
-while i < source_array.length do
-  new_array.push(source_array[i] *2)
-  i +=1
+new_array=[]
+i=0
+while i<source_array.length do
+  new_array.push(source_array[i]*2)
+  i+=1
 end
 return new_array
 end
