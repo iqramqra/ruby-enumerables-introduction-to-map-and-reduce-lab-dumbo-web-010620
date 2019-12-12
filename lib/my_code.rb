216fdc4c7dd9ecdb9a218d33d395db022df7aaae
Iqra Masroor <masrooriqra@gmail.com>
2019-12-12 20:07:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -30,11 +30,11 @@ return new_array
 end
 
 def map_to_square(source_array)
-  new_array = []
-  i = 0
-  while i < source_array.length do
-    new_array.push(source_array[i] **2)
-    i +=1
+  new_array=[]
+  i=0
+  while i<source_array.length do
+    new_array.push(source_array[i]**2)
+    i+=1
   end
   return new_array
 end
